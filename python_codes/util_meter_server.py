7183b64ba9c8e3ab30ce4456eba8b19da6e6dd53
yuanhui <yuanhui@ut.cn>
2021-08-26 11:33:52 +0800

    del

--- python_codes/util_meter_server.py
+++ python_codes/util_meter_server.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify
 from io import BytesIO
 import base64
 import json
-from app_meter_recognition import meter_rec, meter_loc
+# from app_meter_recognition import meter_rec, meter_loc
+from app_inspection_pointer import inspection_pointer
 
 app = Flask(__name__)
 
@@ -38,6 +39,21 @@ def meter_location():
     except:
         res = {'code': 1, 'msg': 'Request exception!','data': []}
         return jsonify(res)
+
+@app.route('/inspection_pointer_server/', methods=['POST'])
+def inspection_pointer_server():
+    # 检测结果
+    if request.method != 'POST':
+        res = {'code': 1, 'msg': 'Only POST requests are supported!', 'data': []}  # 请求失败
+        return jsonify(res)
+    # 获取请求的json数据
+    data = json.loads(request.get_data(as_text=True))
+    res = inspection_pointer(data)
+    print("inspection_pointer result:")
+    print("-----------------------------------------------")
+    print(res)
+    print("----------------------------------------------")
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
