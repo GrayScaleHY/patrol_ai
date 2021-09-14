6114d56fc96fe2e36b1d9ece9a6383f0560af992
yuanhui <yuanhui@ut.cn>
2021-09-14 14:25:48 +0800

    add color_list function

--- python_codes/lib_help_base.py
+++ python_codes/lib_help_base.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 
 class Logger(object):
@@ -17,3 +18,28 @@ class Logger(object):
  
     def flush(self):
         pass
+
+def color_list(c_size):
+    """
+    生成一个颜色列表。
+    """
+    color_map = {
+        # 0: (0, 0, 0), # 黑色
+        # 1: (255, 255, 255), # 白色
+        0: (255, 0, 0), # 红色
+        1: (0, 255, 0), # 绿色
+        2: (0, 0, 255), # 蓝色
+        3: (0, 255, 255), # 青色
+        4: (255, 0, 255), # 洋红色
+        5: (255, 255, 0), # 黄色
+    }
+    colors = []
+    if c_size <= len(color_map):
+        for i in range(c_size):
+            colors.append(color_map[i])
+    else:
+        colors = [color_map[c] for c in color_map]
+        for i in range(c_size - len(color_map)):
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            colors.append(color)
+    return colors
