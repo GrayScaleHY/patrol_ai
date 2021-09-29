673b02fe42fe1d1312d3b5739e2f43c029f6b432
yuanhui <yuanhui@ut.cn>
2021-09-29 09:53:44 +0800

    update

--- python_codes/lib_image_ops.py
+++ python_codes/lib_image_ops.py
@@ -228,15 +228,16 @@ def img_chinese(img, text, coor, color=(0, 255, 0), size=20):
     return:
         img
     """
+    color_real = (color[2], color[1], color[0])
     if (isinstance(img, np.ndarray)):  # 判断是否OpenCV图片类型
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     # 创建一个可以在给定图像上绘图的对象
     draw = ImageDraw.Draw(img)
     # 字体的格式
     fontStyle = ImageFont.truetype(
-        "../c_codes/simsun.ttc", size, encoding="utf-8")
+        "../c_codes/simsun.ttc", size, index=1, encoding="utf-8")
     # 绘制文本
-    draw.text(coor, text, color, font=fontStyle)
+    draw.text(coor, text, color_real, font=fontStyle)
     # 转换回OpenCV格式
     return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
