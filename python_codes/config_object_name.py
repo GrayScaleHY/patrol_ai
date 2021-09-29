5e3ae37b0a58e2fb4d9081cb8df94d2add6b23d8
yuanhui <yuanhui@ut.cn>
2021-09-29 09:54:53 +0800

    名字字典

---
+++ python_codes/config_object_name.py
@@ -0,0 +1,33 @@
+OBJECT_MAP = {
+    "air_switch": {
+        "name": "空气开关",
+        "air_switch_on": "合闸",
+        "air_switch_off": "分闸"
+        },
+    "pressplate":{
+        "name": "压板",
+        "pressplate_on": "投入",
+        "pressplate_off": "退出"
+    },
+    "led": {
+        "name": "led灯",
+        "green_on": "绿开",
+        "green_off": "绿开",
+        "red_on": "红开",
+        "red_off": "红关",
+        "white_on": "白开",
+        "white_off": "白关",
+        "yellow_on": "黄开",
+        "yellow_off": "黄关",
+        "black_off": "黑关",
+    },
+    "fanpaiqi": {
+        "fanpaiqi_red": "合闸",
+        "fanpaiqi_green": "分闸",
+        "fanpaiqi_jiedi": "接地"
+    },
+    "meter": {
+        "meter": "表记",
+        "pointer": "指针"
+    }
+}
