b28d74028aeb4254d16c44a1ee42394bb063f9f3
yuanhui <yuanhui@ut.cn>
2021-09-29 15:51:59 +0800

    update

--- python_codes/config_object_name.py
+++ python_codes/config_object_name.py
@@ -1,4 +1,8 @@
 OBJECT_MAP = {
+    "meter": {
+        "meter": "表记",
+        "pointer": "指针"
+    },
     "air_switch": {
         "name": "空气开关",
         "air_switch_on": "合闸",
@@ -10,7 +14,7 @@ OBJECT_MAP = {
         "pressplate_off": "退出"
     },
     "led": {
-        "name": "led灯",
+        "name": "LED灯",
         "green_on": "绿开",
         "green_off": "绿开",
         "red_on": "红开",
@@ -22,12 +26,80 @@ OBJECT_MAP = {
         "black_off": "黑关",
     },
     "fanpaiqi": {
+        "name": "翻拍器",
         "fanpaiqi_red": "合闸",
         "fanpaiqi_green": "分闸",
         "fanpaiqi_jiedi": "接地"
     },
-    "meter": {
-        "meter": "表记",
-        "pointer": "指针"
+    "action_recognition":{
+        "name": "行为识别",
+        "smoking": "抽烟",
+        "fall": "摔倒",
+        "open_door": "开门",
+        "fight": "打架"
+    },
+    "digital": {
+        "name": "led数字",
+        '0': '0',
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+        '0': '0',
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+        '-': '-'
+    },
+    "counter": {
+        "name": "动作次数器",
+        '0': '0',
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9'
+    },
+    "fire_smoke": {
+        "name": "烟火",
+        'fire': '火',
+        'smoke': '烟',
+
+    },
+    "helmet": {
+        "name": "安全帽",
+        'person': '未佩戴安全帽',
+        'hat': '已佩戴安全帽',
+        'leifenghat': '雷锋帽',
+    },
+    "insulator": {
+        "name": "绝缘体",
+        "insulator": "绝缘"
+    },
+    "sitecar": {
+        "name": "机动车",
+        'crane': '吊车',
+        'excavator': '挖掘机'
+    },
+    "yingguangfu": {
+        "name": "荧光服",
+        'yingguangfu': '已穿荧光服',
+        'no_yingguangfu': '未穿荧光服',
+        'gongzuofu': '已穿工作服'
     }
 }
