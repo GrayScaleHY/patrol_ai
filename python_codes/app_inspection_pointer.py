c1400a58bd1fa09ae860b3d3c2776f5502c1e19e
yuanhui <yuanhui@ut.cn>
2021-09-30 17:08:17 +0800

    添加指针筛选机制

--- python_codes/app_inspection_pointer.py
+++ python_codes/app_inspection_pointer.py
@@ -2,13 +2,15 @@ import os
 import cv2
 import time
 import json
-import json
 import lib_image_ops
+import numpy as np
 from lib_inference_yolov5 import load_yolov5_model, inference_yolov5
 from lib_analysis_meter import angle_scale, segment2angle, angle2sclae, draw_result
 from lib_inference_mrcnn import load_maskrcnn_model, inference_maskrcnn, contour2segment, intersection_arc
 from app_inspection_disconnector import sift_match, convert_coor
+from lib_image_ops import img_chinese
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 ## 加载模型
 yolov5_meter = load_yolov5_model("/data/inspection/yolov5/meter.pt") # 加载仪表yolov5模型
@@ -63,7 +65,7 @@ def cal_base_angle(coordinates, segment):
     config = [cfg]
     out_cfg = angle_scale(config)[0]
     seg_ang = segment2angle((segment[0], segment[1]), (segment[2], segment[3]))
-    val = round(angle2sclae(out_cfg, seg_ang),3)
+    val = angle2sclae(out_cfg, seg_ang)
     return val
 
 
@@ -97,9 +99,103 @@ def cal_base_scale(coordinates, segment):
     config = [{str(angle_1): scale_1, str(angle_2): scale_2}]
     out_cfg = angle_scale(config)[0]
     seg_ang = segment2angle((seg[0], seg[1]), (seg[2], seg[3]))
-    val = round(angle2sclae(out_cfg, seg_ang),3)
+    val = angle2sclae(out_cfg, seg_ang)
     return val
 
+
+def get_input_data(input_data):
+    """
+    提取input_data中的信息。
+    return:
+        img_tag: 目标图片数据
+        img_ref: 模板图片数据
+        pointers_ref: 坐标点, 结构为{"center": [100，200], "-0.1": [200，300], "0.9": [400，500]}
+        roi: 感兴趣区域, 结构为[xmin, ymin, xmax, ymax]
+        number: None 或者 指针数量
+        length: None 或者 指针长短
+        width: None 或者 指针粗细
+        color: None 或者 指针颜色
+        dp: 3 或者 需要保留的小数位
+    """
+    img_tag = lib_image_ops.base642img(input_data["image"])
+    img_ref = lib_image_ops.base642img(input_data["config"]["img_ref"])
+
+    W = img_ref.shape[1]; H = img_ref.shape[0]
+
+    ## 点坐标
+    pointers = input_data["config"]["pointers"]
+    pointers_ref = {}
+    for coor in pointers:
+        pointers_ref[coor] = [int(pointers[coor][0] * W), int(pointers[coor][1] * H)]
+
+    ## 感兴趣区域
+    if "bboxes" in input_data["config"]:
+        roi = input_data["config"]["bboxes"]["roi"]
+        roi = [int(roi[0]*W), int(roi[1]*H), int(roi[2]*W), int(roi[3]*H),]
+    else:
+        roi = None
+    
+    ## 其他信息
+    if "number" in input_data["config"]:
+        number = input_data["config"]["number"]
+    else:
+        number = None
+    if "length" in input_data["config"]:
+        length = input_data["config"]["length"]
+    else:
+        length = None
+    if "color" in input_data["config"]:
+        color = input_data["config"]["color"]
+    else:
+        color = None
+    if "width " in input_data["config"]:
+        width = input_data["config"]["width"]
+    else:
+        width = None
+    if "dp " in input_data["config"]:
+        dp = input_data["config"]["dp"]
+    else:
+        dp = 3
+    
+    return img_tag, img_ref, pointers_ref, roi, number, length, width, color, dp
+
+def select_pointer(img, bboxes, number, length, width, color):
+    """
+    根据指针长短，粗细，颜色来筛选指针
+    返回index
+    """
+    if number is None or number == 1 or len(bboxes) <= 1:
+        return 0
+    bboxes = bboxes[:int(number)]
+    if length is not None:
+        lengths = [(a[2]-a[1])**2 + (a[3]-a[0])**2 for a in bboxes]
+        if length == 0:
+            return lengths.index(min(lengths))
+        elif length == 2:
+            return lengths.index(max(lengths))
+        else:
+            lengths.pop(lengths.index(min(lengths)))
+            return lengths.index(min(lengths))
+    elif width is not None:
+        widths = [min(a[2]-a[1], a[3]-a[0]) for a in bboxes]
+        if length == 0:
+            return widths.index(min(widths))
+        elif length == 2:
+            return widths.index(max(widths))
+        else:
+            widths.pop(widths.index(min(widths)))
+            return widths.index(min(widths))
+    elif color is not None:
+        color_list = [[0,0,0],[255,255,255],[0,0,255]] # 黑，白，红
+        c_ref = np.array(color_list[color], dtype=float)
+        d_list = []
+        for b in bboxes:
+            c_tag = np.array(img[int((b[0] + b[2]) / 2), int((b[1] + b[3]) / 2), :], dtype=float)
+            d_list.append(np.linalg.norm(c_ref - c_tag))
+        return d_list.index(min(d_list))
+    else:
+        return 0
+    
 
 def inspection_pointer(input_data):
 
@@ -118,92 +214,100 @@ def inspection_pointer(input_data):
         return out_data
     
     ## 提取输入请求信息
-    img_tag = lib_image_ops.base642img(input_data["image"])
-    img_ref = lib_image_ops.base642img(input_data["config"]["img_ref"])
-    coordinates = input_data["config"]["coordinates"]
-    coordinates_ref = {}
-    W = img_ref.shape[1]
-    H = img_ref.shape[0]
-    for coor in coordinates:
-        coordinates_ref[coor] = [int(coordinates[coor][0] * W), int(coordinates[coor][1] * H)]
+    img_tag, img_ref, pointers_ref, roi, number, length, width, color, dp= get_input_data(input_data)
 
     ## 将输入请求信息可视化
     img_tag_ = img_tag.copy()
     img_ref_ = img_ref.copy()
     cv2.imwrite(os.path.join(save_path, "img_tag.jpg"), img_tag_)
     cv2.imwrite(os.path.join(save_path, "img_ref.jpg"), img_ref_)
-    for scale in coordinates_ref:  # 将坐标点标注在图片上
-        coor = coordinates_ref[scale]
+    for scale in pointers_ref:  # 将坐标点标注在图片上
+        coor = pointers_ref[scale]
         cv2.circle(img_ref_, (int(coor[0]), int(coor[1])), 2, (255, 0, 0), 8)
         cv2.putText(img_ref_, str(scale), (int(coor[0])-5, int(coor[1])),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), thickness=2)
+    if roi is not None:   ## 如果配置了感兴趣区域，则画出感兴趣区域
+        cv2.rectangle(img_ref_, (int(roi[0]), int(roi[1])),
+                    (int(roi[2]), int(roi[3])), (0, 0, 255), thickness=2)
+        cv2.putText(img_ref_, "roi", (int(roi[0])-5, int(roi[1])-5),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), thickness=2)
     cv2.imwrite(os.path.join(save_path, "img_ref_cfg.jpg"), img_ref_)
+
+    ## 如果没有配置roi，则自动识别表盘作为roi
+    if roi is None:
+        bbox_ref = inference_yolov5(yolov5_meter, img_ref, resize=640)
+        if len(bbox_ref) > 0:
+            roi = bbox_ref[0]["coor"]
+        else:
+            roi = [0,0, img_ref.shape[1], img_ref.shape[0]]
         
-    ## 使用yolov5定位参考图和目标图的表盘。
-    bbox_ref = inference_yolov5(yolov5_meter, img_ref, resize=640)
-    if len(bbox_ref) > 0:
-        coor_ref = bbox_ref[0]["coor"]
-    else:
-        coor_ref = [0,0, img_ref.shape[1], img_ref.shape[0]]
-    meter_ref = img_ref[int(coor_ref[1]): int(coor_ref[3]), int(coor_ref[0]): int(coor_ref[2])]
-
-    bbox_tag = inference_yolov5(yolov5_meter, img_tag, resize=640)
-    if len(bbox_tag) > 0:
-        coor_tag = bbox_tag[0]["coor"]
-    else:
-        coor_tag = [0,0, img_tag.shape[1], img_tag.shape[0]]
-    meter_tag = img_tag[int(coor_tag[1]): int(coor_tag[3]), int(coor_tag[0]): int(coor_tag[2])]
+    M = sift_match(img_ref, img_tag, ratio=0.5, ops="Perspective")
     
-    # 用maskrcnn检测指针轮廓并且拟合成线段。
-    contours, boxes = inference_maskrcnn(maskrcnn_pointer, meter_tag)
-    segments = contour2segment(contours, boxes)
-
-    ## 根据与表盘中心的距离更正segment的头尾
-    xo = meter_tag.shape[1] / 2; yo = meter_tag.shape[0] / 2
-    for i, s in enumerate(segments):
-        if (s[0]-xo)**2+(s[1]-yo)**2 > (s[2]-xo)**2+(s[3]-yo)**2:
-            segments[i] = [s[2], s[3], s[0], s[1]]
-
-    ## 将segments还原到原图的坐标
-    dx = coor_tag[0]; dy = coor_tag[1]
-    segments = [[s[0]+dx, s[1]+dy, s[2]+dx, s[3]+dy] for s in segments]
+    if M is None:
+        roi_tag = [0,0, img_tag.shape[1], img_tag.shape[0]]
+    else:
+        coors = [(roi[0],roi[1]), (roi[2],roi[1]), (roi[2],roi[3]), (roi[0],roi[3])]
+        coors_ = []
+        for coor in coors:
+            coors_.append(list(convert_coor(coor, M)))
+        coors_ = np.array(coors_, dtype=int)
+        roi_tag = [np.min(coors_[:,0]), np.min(coors_[:,1]), np.max(coors_[:,0]), np.max(coors_[:,1])]
+    img_roi = img_tag[int(roi_tag[1]): int(roi_tag[3]), int(roi_tag[0]): int(roi_tag[2])]
 
     ## 使用映射变换矫正目标图，并且转换坐标点。
-    M = sift_match(meter_ref, meter_tag, ratio=0.5, ops="Perspective")
-    coordinates_tag = conv_coor(coordinates_ref, M, coor_ref[:2], coor_tag[:2])
+    pointers_tag = conv_coor(pointers_ref, M)
 
-    segment_real = []
-    vals = []
-    for segment in segments:
-        if M is not None:
-            val = cal_base_scale(coordinates_tag, segment)
-        else:
-            val = cal_base_angle(coordinates_tag, segment)
-        if val is not None:
-            vals.append(val)
-            segment_real.append(segment)
-    if len(vals) > 0:
-        cfg = {"type": "pointer", "values": vals, "segments": segment_real, "bbox": coor_tag}
+    ## 将矫正偏移的信息写到图片中
+    s = (roi_tag[2] - roi_tag[0]) / 200 # 根据框子大小决定字号和线条粗细。
+    cv2.rectangle(img_tag_, (int(roi_tag[0]), int(roi_tag[1])),
+                    (int(roi_tag[2]), int(roi_tag[3])), (0, 0, 255), thickness=round(s*2))
+    cv2.putText(img_tag_, "roi", (int(roi_tag[0]), int(roi_tag[1]-s)),
+                    cv2.FONT_HERSHEY_SIMPLEX, s, (0, 0, 255), thickness=round(s))
+    for scale in pointers_tag:
+        coor = pointers_tag[scale]
+        cv2.circle(img_tag_, (int(coor[0]), int(coor[1])), round(s), (255, 0, 255), 8)
+        cv2.putText(img_tag_, str(scale), (int(coor[0]+5), int(coor[1]-5)),
+                    cv2.FONT_HERSHEY_SIMPLEX, round(s/4), (255, 0, 255), thickness=round(s))
 
-        out_data["data"] = cfg
+    # 用maskrcnn检测指针轮廓并且拟合成线段.
+    contours, boxes = inference_maskrcnn(maskrcnn_pointer, img_roi)
+    segments = contour2segment(contours, boxes)
+
+    if len(segments) == 0:
+        out_data["msg"] = out_data["msg"] + "Can not find pointer; "
+        cv2.imwrite(os.path.join(save_path, "img_tag_cfg.jpg"), img_tag_)
+        return out_data
+
+    ## 筛选指针
+    i = select_pointer(roi_tag, boxes, number, length, width, color)
+    seg = segments[i]
+
+    ## 根据与表盘中心的距离更正segment的头尾
+    xo = img_roi.shape[1] / 2; yo = img_roi.shape[0] / 2
+    if (seg[0]-xo)**2+(seg[1]-yo)**2 > (seg[2]-xo)**2+(seg[3]-yo)**2:
+        seg = [seg[2], seg[3], seg[0], seg[1]]
+
+    ## 将segments还原到原图的坐标
+    dx = roi_tag[0]; dy = roi_tag[1]
+    seg = [seg[0]+dx, seg[1]+dy, seg[2]+dx, seg[3]+dy]
+
+    if M is not None:
+        val = cal_base_scale(pointers_tag, seg)
+    else:
+        val = cal_base_angle(pointers_tag, seg)
+    val = round(val, dp)
+    
+    seg = [float(seg[0]), float(seg[1]), float(seg[2]), float(seg[3])]
+    roi_tag = [float(roi_tag[0]), float(roi_tag[1]), float(roi_tag[2]), float(roi_tag[3])]
+    out_data["data"] = {"type": "pointer", "values": val, "segment": seg, "bbox": roi_tag}
+    if val == None:
+        out_data["msg"] = out_data["msg"] + "Can not find ture pointer; "
 
     ## 可视化最终计算结果
-    s = (coor_tag[2] - coor_tag[0]) / 200 # 根据框子大小决定字号和线条粗细。
-    cv2.rectangle(img_tag_, (int(coor_tag[0]), int(coor_tag[1])),
-                    (int(coor_tag[2]), int(coor_tag[3])), (0, 0, 255), thickness=round(s*2))
-    cv2.putText(img_tag_, "meter", (int(coor_tag[0])-5, int(coor_tag[1])-5),
-                    cv2.FONT_HERSHEY_SIMPLEX, s/2, (0, 0, 255), thickness=round(s))
-    for seg in segments[:1]:
-        cv2.line(img_tag_, (int(seg[0]), int(seg[1])), (int(seg[2]), int(seg[3])), (0, 255, 0), round(s))
-    for i, val in enumerate(vals[:1]):
-        cv2.putText(img_tag_, str(val), (int(segment_real[i][2])-5, int(segment_real[i][3])),
-                    cv2.FONT_HERSHEY_SIMPLEX, s, (0, 255, 0), thickness=round(s*2))
-    for scale in coordinates_tag:
-        coor = coordinates_tag[scale]
-        cv2.circle(img_tag_, (int(coor[0]), int(coor[1])), round(s), (255, 0, 255), 8)
-        cv2.putText(img_tag_, str(scale), (int(coor[0]+s), int(coor[1]-s)),
-                    cv2.FONT_HERSHEY_SIMPLEX, s/4, (255, 0, 255), thickness=round(s/2))
-
+    cv2.line(img_tag_, (int(seg[0]), int(seg[1])), (int(seg[2]), int(seg[3])), (0, 255, 0), round(s))
+    cv2.putText(img_tag_, str(val), (int(seg[2])-5, int(seg[3])),
+                cv2.FONT_HERSHEY_SIMPLEX, round(s), (0, 255, 0), thickness=round(s*2))
+    
     cv2.imwrite(os.path.join(save_path, "img_tag_cfg.jpg"), img_tag_)
 
     ## 输出可视化结果的图片。
@@ -215,27 +319,32 @@ def inspection_pointer(input_data):
     return out_data
 
 def main():
-    img_ref_file = "/home/yh/image/python_codes/test/test1.jpg"
-    img_tag_file = "/home/yh/image/python_codes/test/test1.jpg"
-    coordinates = {"center": [1074, 609], "-0.1": [919, 796], "0": [854, 709], "0.2": [864, 513], "0.4": [1058, 392], "0.6": [1256, 476], "0.8": [1312, 670], "0.9": [1253, 759]}
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    img_ref_file = "test/img_ref.jpg"
+    img_tag_file = "test/img_tag.jpg"
+    pointers = {"center": [1007, 405], "0": [1008, 319], "2": [1139, 385], "3": [1133, 447], "5": [995, 489], "7": [865, 426], "8": [871, 363], "9": [937, 322]}
+    bboxes = {"roi": [805, 256, 1217, 556]}
     img_ref = cv2.imread(img_tag_file)
     W = img_ref.shape[1]; H = img_ref.shape[0]
-    for t in coordinates:
-        coor = [coordinates[t][0]/W, coordinates[t][1]/H]
-        coordinates[t] = coor
+    for t in pointers:
+        pointers[t] = [pointers[t][0]/W, pointers[t][1]/H]
+    for b in bboxes:
+        bboxes[b] = [bboxes[b][0]/W, bboxes[b][1]/H, bboxes[b][2]/W, bboxes[b][3]/H]
     
     img_tag = lib_image_ops.img2base64(cv2.imread(img_tag_file))
     img_ref = lib_image_ops.img2base64(cv2.imread(img_ref_file))
-    input_data = {"image": img_tag, "config": {"img_ref": img_ref, "coordinates": coordinates}, "type": "pointer"}
+    config = {
+        "img_ref": img_ref, 
+        "number": 1, 
+        "pointers": pointers,
+        "length": 0, 
+        "width": 0, 
+        "color": 0, 
+        "bboxes": bboxes
+    }
+    input_data = {"image": img_tag, "config": config, "type": "pointer"}
     out_data = inspection_pointer(input_data)
-    # print(out_data)
-    # with open(img_ref_file, "rb") as imageFile:
-    #     img_1 = imageFile.read()
-    # img_2 = base64.b64encode(img_1).decode('utf-8')
-    # img_3 = base64.b64decode(str(img_2))
-    # img_4 = np.fromstring(img_3, np.uint8)
-    # img_5 = cv2.imdecode(img_4, cv2.IMREAD_COLOR)
-    # print("a")
+
 
 if __name__ == '__main__':
     main()
