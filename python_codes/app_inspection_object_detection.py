517219a8c9a6fb1ca4fc49f293db3a1057f20341
yuanhui <yuanhui@ut.cn>
2021-10-26 11:10:08 +0800

    update

--- python_codes/app_inspection_object_detection.py
+++ python_codes/app_inspection_object_detection.py
@@ -22,20 +22,22 @@ def get_input_data(input_data):
     img_tag = base642img(input_data["image"])
 
     ## 是否有模板图
-    if "img_ref" not in input_data["config"] or input_data["config"]["img_ref"] == -1:
-        img_ref = None
-    else:
-        img_ref = base642img(input_data["config"]["img_ref"])
+    img_ref = None
+    if "img_ref" in input_data["config"]:
+        if isinstance(input_data["config"]["img_ref"], str):
+            img_ref = base642img(input_data["config"]["img_ref"])      
         
-
     ## 感兴趣区域
-    if "bboxes" not in input_data["config"] or input_data["config"]["bboxes"] == -1:
-        roi = None
-    else:
-        W = img_ref.shape[1]; H = img_ref.shape[0]
-        roi = input_data["config"]["bboxes"]["roi"]
-        roi = [int(roi[0]*W), int(roi[1]*H), int(roi[2]*W), int(roi[3]*H)]
-        
+    roi = None # 初始假设
+    if "bboxes" in input_data["config"]:
+        if isinstance(input_data["config"]["bboxes"], dict):
+            if "roi" in input_data["config"]["bboxes"]:
+                if isinstance(input_data["config"]["bboxes"]["roi"], list):
+                    if len(isinstance(input_data["config"]["bboxes"]["roi"])) == 4:
+                        W = img_ref.shape[1]; H = img_ref.shape[0]
+                        roi = input_data["config"]["bboxes"]["roi"]
+                        roi = [int(roi[0]*W), int(roi[1]*H), int(roi[2]*W), int(roi[3]*H)]   
+
     return img_tag, img_ref, roi
 
 
@@ -166,16 +168,18 @@ def inspection_object_detection(input_data):
 
 
 if __name__ == '__main__':
-    tag_file = "/home/yh/inspection/python_codes/inspection_result/led/09-29-20-01-59/img_ref.jpg"
-    ref_file = "test/p2.jpg"
-    img_tag = img2base64(cv2.imread(tag_file))
-    img_ = cv2.imread(ref_file)
-    img_ref = img2base64(img_)
-    ROI = [1249, 1154, 1885, 1400]
-    W = img_.shape[1]; H = img_.shape[0]
-    roi = [ROI[0]/W, ROI[1]/H, ROI[2]/W, ROI[3]/H]
+    # tag_file = "/home/yh/inspection/python_codes/inspection_result/led/09-29-20-01-59/img_ref.jpg"
+    # ref_file = "test/p2.jpg"
+    # img_tag = img2base64(cv2.imread(tag_file))
+    # img_ = cv2.imread(ref_file)
+    # img_ref = img2base64(img_)
+    # ROI = [1249, 1154, 1885, 1400]
+    # W = img_.shape[1]; H = img_.shape[0]
+    # roi = [ROI[0]/W, ROI[1]/H, ROI[2]/W, ROI[3]/H]
 
-    input_data = {"image": img_tag, "config":{}, "type": "led"} # "img_ref": img_ref, "bboxes": {"roi": roi}
+    # input_data = {"image": img_tag, "config":{}, "type": "led"} # "img_ref": img_ref, "bboxes": {"roi": roi}
+    f = open("/home/yh/image/python_codes/inspection_result/meter/test/input_data.json", "r", encoding='utf-8')
+    input_data = json.load(f)
     out_data = inspection_object_detection(input_data)
     print("inspection_object_detection result:")
     print("-----------------------------------------------")
