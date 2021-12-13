08032d2eb77ff62e0b64f0ed848e8739479f35cf
yuanhui <yuanhui@ut.cn>
2021-12-13 10:05:10 +0800

    update

--- python_codes/util_inspection_server.py
+++ python_codes/util_inspection_server.py
@@ -128,6 +128,7 @@ def inspection_level_gauge_server():
 @app.route('/inspection_meter/', methods=['POST'])
 @app.route('/inspection_arrow/', methods=['POST'])
 @app.route('/inspection_rotary_switch/', methods=['POST'])
+@app.route('/inspection_door/', methods=['POST'])
 def inspection_object():
     if request.method != 'POST':
         res = {'code': 1, 'msg': 'Only POST requests are supported!', 'data': []}
