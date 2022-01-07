fa408584b1dfca9d6fd11e8170ca6e297a3695cf
yuanhui <yuanhui@ut.cn>
2022-01-07 15:33:22 +0800

    update

--- python_codes/app_inspection_pointer.py
+++ python_codes/app_inspection_pointer.py
@@ -16,6 +16,19 @@ yolov5_meter = load_yolov5_model("/data/inspection/yolov5/meter.pt") # 加载仪
 maskrcnn_pointer = load_maskrcnn_model("/data/inspection/maskrcnn/pointer.pth") # 加载指针的maskrcnn模型
 
 
+def is_include(sub_box, par_box, srate=0.8):
+    sb = sub_box; pb = par_box
+    sb = [min(sb[0],sb[2]), min(sb[1],sb[3]), max(sb[0],sb[2]), max(sb[1],sb[3])]
+    pb = [min(pb[0],pb[2]), min(pb[1],pb[3]), max(pb[0],pb[2]), max(pb[1],pb[3])]
+    xmin = max(pb[0], sb[0]); ymin = max(pb[1], sb[1])
+    xmax = min(pb[2], sb[2]); ymax = min(pb[3], sb[3])
+    s_include = (xmax-xmin) * (ymax-ymin)
+    s_box = (sb[2]-sb[0]) * (sb[3]-sb[1])
+    if s_include / s_box >= srate:
+        return True
+    else:
+        return False
+
 def conv_coor(coordinates, M, d_ref=(0,0), d_tag=(0,0)):
     """
     根据偏移矩阵M矫正坐标点。
@@ -167,14 +180,20 @@ def get_input_data(input_data):
     
     return img_tag, img_ref, pointers_ref, roi, number, length, width, color, dp
 
-def select_pointer(img, bboxes, number, length, width, color):
+def select_pointer(img, segments, number, length, width, color):
     """
     根据指针长短，粗细，颜色来筛选指针
     返回index
     """
-    if number is None or number == 1 or len(bboxes) <= 1:
+    if number is None or number == 1 or len(segments) <= 1:
         return 0
-    bboxes = bboxes[:int(number)]
+    
+    segments = segments[:int(number)]
+    bboxes = []
+    for s in segments:
+        bbox = [min(s[0],s[2]), min(s[1],s[3]), max(s[0],s[2]), max(s[1],s[3])]
+        bboxes.append(bbox)
+
     if length is not None:
         lengths = [(a[2]-a[1])**2 + (a[3]-a[0])**2 for a in bboxes]
         if length == 0:
@@ -215,12 +234,12 @@ def inspection_pointer(input_data):
     json.dump(input_data, f, ensure_ascii=False)  # 保存输入信息json文件
     f.close()
 
-    out_data = {"code":0, "data":[], "img_result": "image", "msg": "request sucdess; "} #初始化输出信息
+    out_data = {"code":0, "data":[], "img_result": input_data["image"], "msg": "Request success; "} #初始化输出信息
 
     if input_data["type"] != "pointer":
         out_data["msg"] = out_data["msg"] + "type isn't pointer; "
         return out_data
-    
+
     ## 提取输入请求信息
     img_tag, img_ref, pointers_ref, roi, number, length, width, color, dp= get_input_data(input_data)
 
@@ -231,90 +250,106 @@ def inspection_pointer(input_data):
     cv2.imwrite(os.path.join(save_path, "img_ref.jpg"), img_ref_)
     for scale in pointers_ref:  # 将坐标点标注在图片上
         coor = pointers_ref[scale]
-        cv2.circle(img_ref_, (int(coor[0]), int(coor[1])), 4, (255, 0, 255), 8)
-        cv2.putText(img_ref_, str(scale), (int(coor[0])-5, int(coor[1])),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 255), thickness=4)
+        cv2.circle(img_ref_, (int(coor[0]), int(coor[1])), 1, (255, 0, 255), 8)
+        cv2.putText(img_ref_, str(scale), (int(coor[0]), int(coor[1])-5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), thickness=1)
     if roi is not None:   ## 如果配置了感兴趣区域，则画出感兴趣区域
-        cv2.rectangle(img_ref_, (int(roi[0]), int(roi[1])),
-                    (int(roi[2]), int(roi[3])), (0, 0, 255), thickness=2)
-        cv2.putText(img_ref_, "roi", (int(roi[0])-5, int(roi[1])-5),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), thickness=2)
+        cv2.rectangle(img_ref_, (int(roi[0]), int(roi[1])),(int(roi[2]), int(roi[3])), (255, 0, 255), thickness=1)
+        cv2.putText(img_ref_, "roi", (int(roi[0])-5, int(roi[1])-5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), thickness=1)
     cv2.imwrite(os.path.join(save_path, "img_ref_cfg.jpg"), img_ref_)
 
-    ## 如果没有配置roi，则自动识别表盘作为roi
-    if roi is None:
-        bbox_ref = inference_yolov5(yolov5_meter, img_ref, resize=640)
-        if len(bbox_ref) > 0:
-            roi = bbox_ref[0]["coor"]
-        else:
-            roi = [0,0, img_ref.shape[1], img_ref.shape[0]]
-        
-    M = sift_match(img_ref, img_tag, ratio=0.5, ops="Perspective")
+    ## 识别图中的表盘
+    bboxes = inference_yolov5(yolov5_meter, img_tag, resize=640)
+    if len(bboxes) == 0:
+        bboxes = [{"label": "meter", "coor": [0, 0, img_tag.shape[1],img_tag.shape[0]], "score": 1.0}]
+    roi_tag = bboxes[0]["coor"]
+
+    ## 找到bboxes中的所有指针
+    segments_cfg = []
+    for bbox in bboxes:
+        c = np.array(bbox["coor"],dtype=int)
+        img = img_tag[c[1]:c[3], c[0]:c[2]]
+        contours, boxes, (masks, classes, scores) = inference_maskrcnn(maskrcnn_pointer, img)
+        segments = contour2segment(contours, boxes)
+        for i in range(len(scores)):
+            s = segments[i]
+            seg = [s[0]+c[0], s[1]+c[1], s[2]+c[0], s[3]+c[1]]
+            segments_cfg.append([scores[i]] + seg)
     
-    if M is None:
-        roi_tag = [0,0, img_tag.shape[1], img_tag.shape[0]]
-    else:
-        coors = [(roi[0],roi[1]), (roi[2],roi[1]), (roi[2],roi[3]), (roi[0],roi[3])]
-        coors_ = []
-        for coor in coors:
-            coors_.append(list(convert_coor(coor, M)))
-        xs = [coor[0] for coor in coors_]
-        ys = [coor[1] for coor in coors_]
-        xmin = max(0, min(xs)); ymin = max(0, min(ys))
-        xmax = min(img_tag.shape[1], max(xs)); ymax = min(img_tag.shape[0], max(ys))
-        roi_tag = [xmin, ymin, xmax, ymax]
-    img_roi = img_tag[int(roi_tag[1]): int(roi_tag[3]), int(roi_tag[0]): int(roi_tag[2])]
+    ## 对segments根据得分排序
+    segments_cfg = np.array(segments_cfg, dtype=float)
+    segments_cfg = segments_cfg[np.argsort(segments_cfg[:, 0])]
+    segments = segments_cfg[:,1:].tolist()  # 置信度从小到大排序
+    segments = [segments[-i-1] for i in range(len(segments))] # 置信度从大到小排序
 
-    ## 使用映射变换矫正目标图，并且转换坐标点。
-    pointers_tag = conv_coor(pointers_ref, M)
+    ## 将所有表盘和指针画出来
+    for bbox in bboxes:
+        c = bbox["coor"]
+        cv2.rectangle(img_tag_, (int(c[0]), int(c[1])),(int(c[2]), int(c[3])), (255, 0, 255), thickness=1)
+        cv2.putText(img_tag_, "meter", (int(c[0]), int(c[1])-5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), thickness=1)
+    for seg in segments:
+        cv2.line(img_tag_, (int(seg[0]), int(seg[1])), (int(seg[2]), int(seg[3])), (255, 0, 255), 1)
 
-    ## 将矫正偏移的信息写到图片中
-    s = (roi_tag[2] - roi_tag[0]) / 400 # 根据框子大小决定字号和线条粗细。
-    cv2.rectangle(img_tag_, (int(roi_tag[0]), int(roi_tag[1])),
-                    (int(roi_tag[2]), int(roi_tag[3])), (0, 0, 255), thickness=round(s*2))
-    cv2.putText(img_tag_, "meter", (int(roi_tag[0]), int(roi_tag[1]-s)),
-                    cv2.FONT_HERSHEY_SIMPLEX, s, (0, 0, 255), thickness=round(s)*2)
-    # img_chinese(img_tag_, "表记", (int(roi_tag[0]), int(roi_tag[1])), color=(0, 0, 255), size=20)
-    for scale in pointers_tag:
-        coor = pointers_tag[scale]
-        cv2.circle(img_tag_, (int(coor[0]), int(coor[1])), round(s/4), (255, 0, 255), 8)
-        cv2.putText(img_tag_, str(scale), (int(coor[0])-5, int(coor[1])),
-                    cv2.FONT_HERSHEY_SIMPLEX, s/2, (255, 0, 255), thickness=round(s)) #round(s/4)
-        
+    ## 求出目标图像的感兴趣区域
+    M = sift_match(img_ref, img_tag, ratio=0.5, ops="Perspective")
+    if roi is not None:
+        if M is None:
+            out_data["msg"] = out_data["msg"] + "; Not enough matches are found"
+            roi_tag = roi
+        else:
+            coors = [(roi[0],roi[1]), (roi[2],roi[1]), (roi[2],roi[3]), (roi[0],roi[3])]
+            coors_ = []
+            for coor in coors:
+                coors_.append(list(convert_coor(coor, M)))
+            xs = [coor[0] for coor in coors_]
+            ys = [coor[1] for coor in coors_]
+            xmin = max(0, min(xs)); ymin = max(0, min(ys))
+            xmax = min(img_tag.shape[1], max(xs)); ymax = min(img_tag.shape[0], max(ys))
+            roi_tag = [xmin, ymin, xmax, ymax]
 
-    # 用maskrcnn检测指针轮廓并且拟合成线段.
-    contours, boxes, _ = inference_maskrcnn(maskrcnn_pointer, img_roi)
-    segments = contour2segment(contours, boxes)
+        ## 画出roi_tag
+        c = roi_tag
+        cv2.rectangle(img_tag_, (int(c[0]), int(c[1])),(int(c[2]), int(c[3])), (255,0,0), thickness=1)
+        cv2.putText(img_tag_, "roi", (int(c[0]), int(c[1])),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), thickness=1)
 
+    ## 将不在感兴趣区域的指针筛选出去
+    if roi is not None:
+        segments_r = []
+        for seg in segments:
+            if is_include(seg, roi_tag, srate=0.8):
+                segments_r.append(seg)
+        segments = segments_r
+    
     if len(segments) == 0:
-        out_data["msg"] = out_data["msg"] + "Can not find pointer; "
+        out_data["msg"] = out_data["msg"] + "Can not find pointer in roi; "
         cv2.imwrite(os.path.join(save_path, "img_tag_cfg.jpg"), img_tag_)
-
         return out_data
 
     ## 筛选指针
-    i = select_pointer(roi_tag, boxes, number, length, width, color)
+    i = select_pointer(img_tag, segments, number, length, width, color)
     seg = segments[i]
+    cv2.line(img_tag_, (int(seg[0]), int(seg[1])), (int(seg[2]), int(seg[3])), (0, 255, 0), 2)
+
+    ## 使用映射变换矫正目标图，并且转换坐标点。
+    pointers_tag = conv_coor(pointers_ref, M)
+    for scale in pointers_tag:
+        coor = pointers_tag[scale]
+        cv2.circle(img_tag_, (int(coor[0]), int(coor[1])), 1, (255, 0, 255), 8)
+        cv2.putText(img_tag_, str(scale), (int(coor[0]), int(coor[1])-5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), thickness=1)
 
     ## 根据与表盘中心的距离更正segment的头尾
-    xo = img_roi.shape[1] / 2; yo = img_roi.shape[0] / 2
+    xo = pointers_tag["center"][0]; yo = pointers_tag["center"][1]
     if (seg[0]-xo)**2+(seg[1]-yo)**2 > (seg[2]-xo)**2+(seg[3]-yo)**2:
         seg = [seg[2], seg[3], seg[0], seg[1]]
 
-    ## 将segments还原到原图的坐标
-    dx = roi_tag[0]; dy = roi_tag[1]
-    seg = [seg[0]+dx, seg[1]+dy, seg[2]+dx, seg[3]+dy]
-
-    if M is not None:
-        val = cal_base_scale(pointers_tag, seg)
-    else:
-        val = cal_base_angle(pointers_tag, seg)
+    ## 求指针读数
+    # if M is not None:
+    val = cal_base_scale(pointers_tag, seg)
+    # else:
+    #     val = cal_base_angle(pointers_tag, seg)
 
     if val == None:
         out_data["msg"] = out_data["msg"] + "Can not find ture pointer; "
-        f = open(os.path.join(save_path, "output_data.json"), "w", encoding='utf-8')
-        json.dump(out_data, f, indent=2, ensure_ascii=False)
-        f.close()
+        cv2.imwrite(os.path.join(save_path, "img_tag_cfg.jpg"), img_tag_)
         return out_data
 
     val = round(val, dp)
@@ -322,18 +357,11 @@ def inspection_pointer(input_data):
     roi_tag = [float(roi_tag[0]), float(roi_tag[1]), float(roi_tag[2]), float(roi_tag[3])]
     out_data["data"] = {"type": "pointer", "values": val, "segment": seg, "bbox": roi_tag}
     
-
-    ## 可视化最终计算结果
-    cv2.line(img_tag_, (int(seg[0]), int(seg[1])), (int(seg[2]), int(seg[3])), (0, 255, 0), round(s))
-    cv2.putText(img_tag_, str(val), (int(seg[2])-5, int(seg[3])),
-                cv2.FONT_HERSHEY_SIMPLEX, round(s), (0, 255, 0), thickness=round(s*2))
-    # cv2.line(img_tag_, (int(seg[0]), int(seg[1])), (int(seg[2]), int(seg[3])), (0, 255, 0), 2)
-    # cv2.putText(img_tag_, str(val), (int(seg[2])-5, int(seg[3])),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), thickness=4)
-    
+    ## 输出可视化结果的图片。
+    s = (roi_tag[2] - roi_tag[0]) / 400
+    cv2.putText(img_tag_, str(val), (int(seg[2]), int(seg[3])-5),cv2.FONT_HERSHEY_SIMPLEX, round(s), (0, 255, 0), thickness=round(s*2))
     cv2.imwrite(os.path.join(save_path, "img_tag_cfg.jpg"), img_tag_)
 
-    ## 输出可视化结果的图片。
     f = open(os.path.join(save_path, "output_data.json"), "w", encoding='utf-8')
     json.dump(out_data, f, indent=2, ensure_ascii=False)
     f.close()
@@ -373,7 +401,7 @@ def main():
     #     # "bboxes": bboxes
     # }
     # input_data = {"image": img_tag, "config": config, "type": "pointer"}
-    f = open("/home/yh/image/python_codes/inspection_result/pointer/12-15-13-57-12/input_data.json","r", encoding='utf-8')
+    f = open("/home/yh/image/python_codes/inspection_result/input_data.json","r", encoding='utf-8')
     input_data = json.load(f)
     f.close()
     out_data = inspection_pointer(input_data)
