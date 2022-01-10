de9beb2b91ae8ac8b594f4410c26c96883a01b68
yuanhui <yuanhui@ut.cn>
2022-01-10 11:34:08 +0800

    update

--- python_codes/app_inspection_object_detection.py
+++ python_codes/app_inspection_object_detection.py
@@ -10,9 +10,21 @@ import config_object_name
 import numpy as np
 
 def is_include(sub_box, par_box, srate=0.8):
+    
     sb = sub_box; pb = par_box
     sb = [min(sb[0],sb[2]), min(sb[1],sb[3]), max(sb[0],sb[2]), max(sb[1],sb[3])]
     pb = [min(pb[0],pb[2]), min(pb[1],pb[3]), max(pb[0],pb[2]), max(pb[1],pb[3])]
+
+    ## 至少一个点在par_box里面
+    points = [[sb[0],sb[1]], [sb[2],sb[1]], [sb[0],sb[3]], [sb[2],sb[3]]]
+    is_in = False
+    for p in points:
+        if p[0] >= pb[0] and p[0] <= pb[2] and p[1] >= pb[1] and p[1] <= pb[3]:
+            is_in = True
+    if not is_in:
+        return False
+    
+    ## 判断交集占多少
     xmin = max(pb[0], sb[0]); ymin = max(pb[1], sb[1])
     xmax = min(pb[2], sb[2]); ymax = min(pb[3], sb[3])
     s_include = (xmax-xmin) * (ymax-ymin)
@@ -204,7 +216,7 @@ if __name__ == '__main__':
     # roi = [ROI[0]/W, ROI[1]/H, ROI[2]/W, ROI[3]/H]
 
     # input_data = {"image": img_tag, "config":{}, "type": "led"} # "img_ref": img_ref, "bboxes": {"roi": roi}
-    f = open("/home/yh/image/python_codes/test/input_data.json", "r", encoding='utf-8')
+    f = open("/home/yh/image/python_codes/inspection_result/input_data.json", "r", encoding='utf-8')
     input_data = json.load(f)
     out_data = inspection_object_detection(input_data)
     print("inspection_object_detection result:")
