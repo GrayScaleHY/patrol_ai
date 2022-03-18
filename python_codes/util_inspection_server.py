68d5fd29fa921c0b3b13b8c6531e743812cdef87
yuanhui <yuanhui@ut.cn>
2022-03-18 14:42:34 +0800

    update

--- python_codes/util_inspection_server.py
+++ python_codes/util_inspection_server.py
@@ -166,4 +166,4 @@ def inspection_object():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5010, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
