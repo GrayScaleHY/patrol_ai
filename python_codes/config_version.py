8ebcaa841bb49e71052f4f358d30d13f9f0ec405
yuanhui <yuanhui@ut.cn>
2022-04-08 08:41:57 +0800

    update

--- python_codes/config_version.py
+++ python_codes/config_version.py
@@ -1,4 +1,6 @@
 ## 用于存放版本号
 code_version = {
-    "v1": "1.1.1.040715_base"
+    "current_version": "1.1.1.040715_release",
+    "historic_version":["1.1.1.040115_base",
+                        "1.1.1.040715_release"]
 }
