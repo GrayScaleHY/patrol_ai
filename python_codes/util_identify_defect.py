5776cb022129520c0adeaf18fabb95bbaf63c7cc
yuanhui <yuanhui@ut.cn>
2022-04-17 10:29:18 +0800

    update

--- python_codes/util_identify_defect.py
+++ python_codes/util_identify_defect.py
@@ -1,51 +1,25 @@
-from re import S
-from lib_inference_yolov5 import load_yolov5_model, inference_yolov5
-from lib_sift_match import detect_diff, _resize_feat
-from lib_inference_mrcnn import load_maskrcnn_model, inference_maskrcnn, contour2segment, intersection_arc
-from lib_analysis_meter import segment2angle
-import numpy as np
+"""
+用于判别算法的测试。
+"""
 
+from lib_inference_yolov5 import load_yolov5_model, inference_yolov5
+from lib_sift_match import detect_diff, sift_match, correct_offset, sift_create
+from lib_inference_mrcnn import load_maskrcnn_model, inference_maskrcnn, contour2segment
+from lib_analysis_meter import segment2angle
+from lib_image_ops import img_chinese
+import glob
+import os
+import cv2
+import time
+import shutil
+
+yolov5_posun = load_yolov5_model("/data/inspection/yolov5/posun.pt") # 破损类缺陷
+yolov5_rotary_switch = load_yolov5_model("/data/inspection/yolov5/rotary_switch.pt") # 切换把手(旋钮开关)
 yolov5_led = load_yolov5_model("/data/inspection/yolov5/led.pt") # led灯
 yolov5_pressplate = load_yolov5_model("/data/inspection/yolov5/pressplate.pt") # 压板
 yolov5_door = load_yolov5_model("/data/inspection/yolov5/door.pt") # 箱门闭合
 yolov5_meter = load_yolov5_model("/data/inspection/yolov5/meter.pt") # 表盘
 maskrcnn_pointer = load_maskrcnn_model("/data/inspection/maskrcnn/pointer.pth", num_classes=1, score_thresh=0.3) # 加载指针的maskrcnn模型
-
-def indentify_door(img_ref, img_tag):
-    """
-    判断是否为箱门闭合异常
-    """
-    bbox_cfg_tag = inference_yolov5(yolov5_door, img_tag)
-    if len(bbox_cfg_tag) == 0:
-        return []
-    
-    bbox_cfg_ref = inference_yolov5(yolov5_door, img_ref)
-    if len(bbox_cfg_ref) == 0:
-        return []
-
-    ## 取最大的bbox
-    s_max = 0 
-    for cfg in bbox_cfg_tag:
-        c = cfg["coor"]
-        s = (c[2] - c[0]) * (c[3] - c[1])
-        if s > s_max:
-            s_max = s 
-            label_tag = cfg["label"]
-            coor_tag = c
-    
-    s_max = 0 
-    for cfg in bbox_cfg_ref:
-        c = cfg["coor"]
-        s = (c[2] - c[0]) * (c[3] - c[1])
-        if s > s_max:
-            s_max = s 
-            label_ref = cfg["label"]
-    
-    if label_tag == label_ref:
-        return []
-    else:
-        return coor_tag
-
 
 def indentify_pointer(img_ref, img_tag):
     """
@@ -114,171 +88,159 @@ def indentify_pointer(img_ref, img_tag):
     return [xmin, ymin, xmax, ymax]
 
 
-def indentify_pressplate(img_ref, img_tag):
+def identify_yolov5(bbox_cfg_ref, bbox_cfg_tag):
+    """
+    判断ref的目标物信息和tag的目标物信息是否一致，不一致的话返回不一致的目标框列表。
+    args:
+        bbox_cfg_ref: 基准图的yolov5推理信息，格式为[{"label": "", "coor": [x0, y0, x1, y1], "score": float}, {}, ..]
+        bbox_cfg_tag: 待分析图的yolov5推理信息，格式为[{"label": "", "coor": [x0, y0, x1, y1], "score": float}, {}, ..]
+    return:
+        tag_diff: 不一致目标框列表
+    """
+    if len(bbox_cfg_tag) == 0:
+        return []
+    
+    if len(bbox_cfg_ref) == 0:
+        return []
+    
+    ## 判断对应位置的目标物是否标签一致，如果不一致,将目标看放入到tag_diff中。
+    tag_diff = []
+    for cfg_tag in bbox_cfg_tag:
+        c_tag = cfg_tag["coor"]
+        xo = (c_tag[2] + c_tag[0]) / 2; yo = (c_tag[3] + c_tag[1]) / 2
+        for cfg_ref in bbox_cfg_ref:
+            c_ref = cfg_ref["coor"]
+            if c_ref[0] < xo < c_ref[2] and c_ref[1] < yo < c_ref[3]:
+                if cfg_ref["label"] != cfg_tag["label"]:
+                    tag_diff.append(c_tag)
+    
+    if len(tag_diff) == 0:
+        return []
+    else:
+        return tag_diff
+
+def identify_defect(img_ref, feat_ref, img_tag, feat_tag):
+    """
+    判别算法
+    """
+    tag_diff = []
+    img_tag_ = img_tag.copy()
+    img_ref_ = img_ref.copy()
+    
+    ## 基于tag对ref进行矫正
+    M = sift_match(feat_tag, feat_ref, ratio=0.5, ops="Affine")
+    img_ref = correct_offset(img_ref, M)
+
+    # ## 箱门闭合异常判别
+    bbox_cfg_tag = inference_yolov5(yolov5_door, img_tag)
+    if len(bbox_cfg_tag) != 0:
+        bbox_cfg_ref = inference_yolov5(yolov5_door, img_ref)
+        tag_diff = identify_yolov5(bbox_cfg_ref, bbox_cfg_tag)
+        if len(tag_diff) != 0:
+            return tag_diff
+
+    # ## 压板异常判别
     bbox_cfg_tag = inference_yolov5(yolov5_pressplate, img_tag)
-    if len(bbox_cfg_tag) == 0:
-        return []
-    
-    bbox_cfg_ref = inference_yolov5(yolov5_pressplate, img_ref)
-    if len(bbox_cfg_ref) == 0:
-        return []
-    
-    if len(bbox_cfg_ref) != len(bbox_cfg_tag):
-        return []
-    
-    ## 判断对应位置的压板是否有异样， 适用于只有一个压板变化了
-    tag_diff = []
-    for cfg_tag in bbox_cfg_tag:
-        c_tag = cfg_tag["coor"]
-        xo = (c_tag[2] - c_tag[0]) / 2; yo = (c_tag[3] - c_tag[1]) / 2
-        for cfg_ref in bbox_cfg_ref:
-            c_ref = cfg_ref["coor"]
-            if c_ref[0] < xo < c_ref[2] and c_ref[1] < yo < c_ref[3]:
-                if cfg_ref["label"] != cfg_tag["label"]:
-                    tag_diff.append(c_tag)
-    
-    if len(tag_diff) != 1:
-        return []
-    else:
-        return tag_diff[0]
+    if len(bbox_cfg_tag) != 0:
+        bbox_cfg_ref = inference_yolov5(yolov5_pressplate, img_ref)
+        tag_diff = identify_yolov5(bbox_cfg_ref, bbox_cfg_tag)
+        if len(tag_diff) != 0:
+            return tag_diff
 
-    ## 适用于会同时出现多个压板状态变化的情况
-    # bbox_tag_on = []
-    # bbox_tag_off = []
-    # for cfg in bbox_cfg_tag:
-    #     if cfg["label"] == "pressplate_on":
-    #         bbox_tag_on.append(cfg["coor"])
-    #     else:
-    #         bbox_tag_off.append(cfg["coor"])
-    # bbox_ref_on = []
-    # bbox_ref_off = []
-    # for cfg in bbox_cfg_ref:
-    #     if cfg["label"] == "pressplate_on":
-    #         bbox_ref_on.append(cfg["coor"])
-    #     else:
-    #         bbox_ref_off.append(cfg["coor"])
-    
-    # if len(bbox_tag_on) > len(bbox_ref_on) and len(bbox_tag_off) < len(bbox_ref_off):
-    #     bbox_tag = bbox_tag_on
-    #     bbox_ref = bbox_ref_on
-    # elif len(bbox_tag_on) < len(bbox_ref_on) and len(bbox_tag_off) > len(bbox_ref_off):
-    #     bbox_tag = bbox_tag_off
-    #     bbox_ref = bbox_ref_off
-    # else:
-    #     return []
-    
-    # tag_diff = []
-    # if len(bbox_ref) == 0:
-    #     tag_diff = bbox_tag
-    # else:
-    #     for tag in bbox_tag:
-    #         xo = (tag[2] - tag[0]) / 2; yo = (tag[3] - tag[0]) / 2
-    #         for ref in bbox_ref:
-    #             if ref[0] < xo < ref[2] and ref[1] < yo < ref[3]:
-    #                 continue
-    #             if tag not in tag_diff:
-    #                 tag_diff.append(tag)
-    # if len(tag_diff) == 0:
-    #     return []
-    # else:
-    #     tag_diff = np.array(tag_diff, dtype=int)
-    #     xmin = np.min(tag_diff[:,0])
-    #     ymin = np.min(tag_diff[:,1])
-    #     xmax = np.max(tag_diff[:,0])
-    #     ymax = np.max(tag_diff[:,1])
-    #     return [xmin, ymin, xmax, ymax]
-
-
-def indentify_led(img_ref, img_tag):
-    """
-    判别led灯是否异常
-    """
+    ## 指示灯异常判别
     bbox_cfg_tag = inference_yolov5(yolov5_led, img_tag)
-    if len(bbox_cfg_tag) == 0:
-        return []
-    
-    bbox_cfg_ref = inference_yolov5(yolov5_led, img_ref)
-    if len(bbox_cfg_ref) == 0:
-        return []
-    
-    if len(bbox_cfg_ref) != len(bbox_cfg_tag):
-        return []
-    
-    ## 判断对应位置的led灯是否有异样， 适用于只有一个灯变化了
-    tag_diff = []
-    for cfg_tag in bbox_cfg_tag:
-        c_tag = cfg_tag["coor"]
-        xo = (c_tag[2] - c_tag[0]) / 2; yo = (c_tag[3] - c_tag[1]) / 2
-        for cfg_ref in bbox_cfg_ref:
-            c_ref = cfg_ref["coor"]
-            if c_ref[0] < xo < c_ref[2] and c_ref[1] < yo < c_ref[3]:
-                if cfg_ref["label"] != cfg_tag["label"]:
-                    tag_diff.append(c_tag)
-    
-    if len(tag_diff) != 1:
-        return []
-    else:
-        return tag_diff[0]
+    if len(bbox_cfg_tag) != 0:
+        bbox_cfg_ref = inference_yolov5(yolov5_led, img_ref)
+        tag_diff = identify_yolov5(bbox_cfg_ref, bbox_cfg_tag)
+        if len(tag_diff) != 0:
+            return tag_diff
 
+    ## 破损类异常判别
+    bbox_cfg_tag = inference_yolov5(yolov5_posun, img_tag)
+    if len(bbox_cfg_tag) != 0:
+        tag_diff = []
+        for cfg in bbox_cfg_tag:
+            tag_diff.append(cfg["coor"])
+        return tag_diff
 
-def indentify_defect(img_ref, ref, feat_ref, img_tag, tag, feat_tag, rate):
-    
-    ## 箱门闭合异常
-    rec = indentify_door(img_ref, img_tag)
-    if len(rec) != 0:
-        return rec
-    
-    ## 仪表读数变化太大
+    ## 旋钮开关异常判别
+    bbox_cfg_tag = inference_yolov5(yolov5_rotary_switch, img_tag)
+    if len(bbox_cfg_tag) != 0:
+        bbox_cfg_ref = inference_yolov5(yolov5_rotary_switch, img_ref)
+        tag_diff = identify_yolov5(bbox_cfg_ref, bbox_cfg_tag)
+        if len(tag_diff) != 0:
+            return tag_diff
+
+    ## 指针读数变化太大
     rec = indentify_pointer(img_ref, img_tag)
-    if len(rec) != 0:
-        return rec
-    
-    ## 压板
-    rec = indentify_pressplate(img_ref, img_tag)
-    if len(rec) != 0:
-        return rec
+    if len(rec) != 0:  
+        tag_diff = [rec]
+        d_ = rec
+        return tag_diff
 
-    ## led灯颜色变化
-    rec = indentify_led(img_ref, img_tag)
-    if len(rec) != 0:
-        return rec
+    # ## 像素相减类异常
+    rec = detect_diff(img_ref_, feat_ref, img_tag, feat_tag)
+    if len(rec) != 0:  
+        tag_diff = [rec]
+        return tag_diff
     
-    rec = detect_diff(img_ref, ref, feat_ref, img_tag, tag, feat_tag, rate)
-
-    return rec
+    return tag_diff
 
 if __name__ == '__main__':
-    import glob
-    import os
-    import cv2
 
+    in_dir = "test/panbie"  # 判别测试图片存放目录
+    out_dir = "test/panbie_result" # 判别算法输出目录
+    resize_rate = 0.5
 
-    in_dir = "pb_nj"
-    out_dir = "tuilishuju_output"
+    start = time.time()
+
     os.makedirs(out_dir, exist_ok=True)
     for ref_file in glob.glob(os.path.join(in_dir, "*_normal.jpg")):
 
         file_id = os.path.basename(ref_file).split("_")[0]
-        img_ref = cv2.imread(ref_file)
-        ref, rate, feat_ref = _resize_feat(img_ref)
+        img_ref = cv2.imread(ref_file) 
+
+        ## resize, 降低分别率，加快特征提取的速度。
+        H, W = img_ref.shape[:2]
+        img_ref = cv2.resize(img_ref, (int(W * resize_rate), int(H * resize_rate)))
+
+        feat_ref = sift_create(img_ref) # 提取sift特征
 
         for tag_file in glob.glob(os.path.join(in_dir, file_id + "_*.jpg")):
             tag_name = os.path.basename(tag_file)
+
             if tag_file.endswith("normal.jpg"):
                 continue
-            
-            out_file = os.path.join(out_dir, tag_name[:-4] + ".jpg")
+            img_tag = cv2.imread(tag_file)
+
+            H, W = img_tag.shape[:2]  ## resize
+            img_tag = cv2.resize(img_tag, (int(W * resize_rate), int(H * resize_rate)))
+
+            feat_tag = sift_create(img_tag) # 提取sift特征
+
+            tag_diff = identify_defect(img_ref, feat_ref, img_tag, feat_tag) # 判别算法
+
+            ## 将tag_diff还原回原始大小
+            diffs = []
+            for diff in tag_diff:
+                d_ = [int(d / resize_rate) for d in diff]
+                diffs.append(d_)
+            tag_diff = diffs
+
+            ## 将结果写成txt
+            if len(tag_diff) == 0:
+                s = "1," + tag_name + ",0,0\n"
+            else:
+                s = ""
+                count = 0
+                for rec in tag_diff:
+                    count += 1
+                    rec = [str(rec[0]), str(rec[1]), str(rec[2]), str(rec[3])]
+                    s = s + str(count) + "," + tag_name + ",1," + ",".join(rec) + "\n"
+            out_file = os.path.join(out_dir, tag_name[:-4] + ".txt")
             f = open(out_file, "w", encoding='utf-8')
             f.write("ID,PATH,TYPE,XMIN,YMIN,XMAX,YMAX\n")
+            f.write(s)
+            f.close()
 
-            img_tag = cv2.imread(tag_file)
-            tag, rate, feat_tag = _resize_feat(img_tag)
-            rec = indentify_defect(img_ref, ref, feat_ref, img_tag, tag, feat_tag, rate)
-
-            if len(rec) == 0:
-                s = "1," + tag_name +"1"
-            
-            
-
-
-
+    print("spend times:", time.time() - start)
