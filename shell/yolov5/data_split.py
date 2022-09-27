4625302a5350e40e4d79d9ddbca31cfe85391a1e
yuanhui <yuanhui@ut.cn>
2022-09-27 17:04:30 +0800

    修改了data split代码

--- shell/yolov5/data_split.py
+++ shell/yolov5/data_split.py
@@ -1,12 +1,14 @@
 """
-将数据集按90%train和10%val的比例分配。
+将数据集按90%train和10%val的比例分配。并验证label文件是否有内容以及是否有对应的.jpg文件
+使用步骤：
+    1. 最终输出文件夹<data_dir>中新建一个new_data目录，并且将图片和标签放入new_data目录下
+    2. 执行脚本 python data_split.py --dir <data_dir>
 """
 
 import os
 import glob
-import shutil
-import cv2
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -18,65 +20,59 @@ args, unparsed = parser.parse_known_args()
 
 data_path = args.dir #数据根目录
 
-## 断言文件夹中必须包含classes.txt, voc.yaml, yolov5_model.yaml这三个文件夹
-name_ = os.path.basename(data_path)
-classes_file = os.path.join(data_path, "classes.txt")
-data_file = os.path.join(data_path, "voc_" + name_ + ".yaml")
-model_file = os.path.join(data_path, "yolov5_model_" + name_ + ".yaml")
-assert os.path.exists(classes_file), classes_file + " is not exist !"
-assert os.path.exists(data_file), data_file + " is not exist !"
-assert os.path.exists(model_file), model_file + " is not exist !"
+start = time.time()
+## 将所有图片转移到data目录下
+new_data_dir = os.path.join(data_path, "new_data")
+assert os.path.exists(new_data_dir), new_data_dir + " is not exist !"
+for type_ in ["images", "labels"]:
+    for root, dirs, files in os.walk(os.path.join(data_path, type_)):
+        for file_name in files:
+            in_file = os.path.join(root, file_name)
+            out_file = os.path.join(new_data_dir, file_name)
+            if os.path.exists(out_file):
+                os.remove(in_file)
+            else:
+                os.rename(in_file, out_file)
 
-## 将train,val中的图片移动到根目录，然后统一分配
-for root, dirs, files in os.walk(os.path.join(data_path,"images")):
-    for raw_name in files:
-        raw_file = os.path.join(root, raw_name)
-        move_file = os.path.join(data_path, raw_name)
-        if raw_name.endswith(".jpg") or raw_name.endswith(".txt"):
-            if not os.path.exists(move_file):
-                shutil.move(raw_file, move_file)
-
-for root, dirs, files in os.walk(os.path.join(data_path,"labels")):
-    for raw_name in files:
-        raw_file = os.path.join(root, raw_name)
-        move_file = os.path.join(data_path, raw_name)
-        if raw_name.endswith(".jpg") or raw_name.endswith(".txt"):
-            if not os.path.exists(move_file):
-                shutil.move(raw_file, move_file)
-
-## 分配根目录下的文件到train和val文件夹
-label_list = glob.glob(os.path.join(data_path, "*.txt"))
-train_labels = label_list[:int(0.9*len(label_list))]
-val_labels = label_list[int(0.9*len(label_list)):]
-
+## 验证new_data目录下的文件并分配到images和labels中
+os.makedirs(os.path.join(data_path, "images", "train"), exist_ok=True)
+os.makedirs(os.path.join(data_path, "images", "val"), exist_ok=True)
+os.makedirs(os.path.join(data_path, "labels", "train"), exist_ok=True)
+os.makedirs(os.path.join(data_path, "labels", "val"), exist_ok=True)
 count = 0
-for type_ in ["train", "val"]:
-    os.makedirs(os.path.join(data_path, "images", type_), exist_ok=True)
-    os.makedirs(os.path.join(data_path, "labels", type_), exist_ok=True)
+for txt_file in glob.glob(os.path.join(new_data_dir, "*.txt")):
 
-    if type_ == "train":
-        labels = train_labels
+    if count % 10 == 0:
+        type_ = "val"
     else:
-        labels = val_labels
-    
-    for label_file in labels:
-        label_name = os.path.basename(label_file)
-        image_name = label_name[:-4] + ".jpg"
-        image_file = label_file[:-4] + ".jpg"
-        if os.path.exists(image_file):
-            # print(image_file)
-            # os.rename(image_file, os.path.join(data_path, "images", type_ ,image_name))
-            # os.rename(label_file, os.path.join(data_path, "labels", type_ ,label_name))
-            img = cv2.imread(image_file)
-            cv2.imwrite(os.path.join(data_path, "images", type_ ,image_name),img)
-            os.remove(image_file)
-            os.rename(label_file, os.path.join(data_path, "labels", type_ ,label_name))
-            count += 1
-            if count % 100 == 0:
-                print(count, "allocation already.")
+        type_ = "train"
 
-## 将根目录下的多余的jpg文件移动到images目录下
-for jpg_file in glob.glob(os.path.join(data_path,"*.jpg")):
-    jpg_name = os.path.basename(jpg_file)
-    move_file = os.path.join(data_path,"images/train", jpg_name)
-    shutil.move(jpg_file, move_file)
+    img_file = txt_file[:-4] + ".jpg"
+    txt_name = os.path.basename(txt_file)
+    img_name = os.path.basename(img_file)
+
+    ## 若不存在对应的图片，将label文件移到最外层
+    if not os.path.exists(img_file):
+        txt_out = os.path.join(data_path,txt_name)
+        os.rename(txt_file, txt_out)
+        continue
+
+    f = open(txt_file, "r", encoding='utf-8')
+    txt_con = f.read()
+    f.close()
+
+    ## 若label文件内容太少，将label文件和图片文件移到最外层
+    if len(txt_con) > 3:
+        txt_out = os.path.join(data_path, "labels", type_, txt_name)
+        img_out = os.path.join(data_path, "images", type_, img_name)
+        count += 1
+    else:
+        txt_out = os.path.join(data_path, txt_name)
+        img_out = os.path.join(data_path, img_name)
+    
+    os.rename(txt_file, txt_out)
+    os.rename(img_file, img_out)
+
+print("Split data spend time:", time.time() - start)
+
+
