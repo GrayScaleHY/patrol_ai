0fcd9317722fff745f6c14fb26c1fb01213a4706
林端儿 <linduaner@ut.cn>
2022-12-15 08:07:15 +0000

    Update app_ocr.py

--- python_codes/app_ocr.py
+++ python_codes/app_ocr.py
@@ -7,6 +7,7 @@ from lib_sift_match import sift_match, convert_coor, sift_create
 import time
 import json
 import os
+# from config_load_models_var import text_sys
 
 det_model_dir = "/data/PatrolAi/ppocr/ch_PP-OCRv2_det_infer/"
 cls_model_dir = "/data/PatrolAi/ppocr/ch_ppocr_mobile_v2.0_cls_infer/"
@@ -62,7 +63,25 @@ def get_input_data(input_data):
     return img_tag, img_ref, roi
 
 def deal_str(ocr_str):
-    deal_ocr_str=ocr_str[:-1]
+    deal_ocr_str = ocr_str[:-1]
+    deal_ocr_str = deal_ocr_str.replace('C','0')
+    deal_ocr_str = deal_ocr_str.replace('U', '0')
+    deal_ocr_str = deal_ocr_str.replace('D', '0')
+    deal_ocr_str = deal_ocr_str.replace('Q', '0')
+    deal_ocr_str = deal_ocr_str.replace('A', '4')
+    deal_ocr_str = deal_ocr_str.replace('-', '.')
+    deal_ocr_str = deal_ocr_str.replace('·', '.')
+    deal_ocr_str = deal_ocr_str.replace(':', '.')
+    deal_ocr_str = deal_ocr_str.replace(' ', '')
+    if deal_ocr_str=='000':
+        deal_ocr_str='0.00'
+    if deal_ocr_str=='00':
+        deal_ocr_str='0.0'
+    if deal_ocr_str=='0.':
+        deal_ocr_str = '0.0'
+    deal_ocr_str = deal_ocr_str + 'A'
+    if '.' not in deal_ocr_str and deal_ocr_str[0]=='0':
+        deal_ocr_str = deal_ocr_str[0] + '.' + deal_ocr_str[1:]
     return deal_ocr_str
 
 def ocr_digit_detection(input_data):
@@ -70,7 +89,7 @@ def ocr_digit_detection(input_data):
     ## 将输入请求信息可视化
     TIME_START = time.strftime("%m-%d-%H-%M-%S") + "_"
     save_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    save_path = os.path.join(save_path, "result_patrol")
+    save_path = os.path.join(save_path, "result_patrol", input_data["type"])
     os.makedirs(save_path, exist_ok=True)
     f = open(os.path.join(save_path, TIME_START + "input_data.json"), "w")
     json.dump(input_data, f, ensure_ascii=False)  # 保存输入信息json文件
@@ -136,35 +155,73 @@ def ocr_digit_detection(input_data):
     srcImg[:, :, :] = 0
     srcImg[ymin:ymax, xmin:xmax] = img_tag_roi
 
+    # from PIL import ImageEnhance
+    # # 对比度增强
+    # enh_con = ImageEnhance.Contrast(srcImg)
+    # contrast = 3
+    # srcImg = enh_con.enhance(contrast)
+
     result = inference_ppocr(srcImg, text_sys)
     print("------------------------------")
     print(result)
     print(roi_tag)
 
     ycenter=0.5*(roi_tag[1]+roi_tag[3])
-    out_data["data"] = {"type": "digital", "values": ['0', '0' ,'0'], "bboxes": [[0, 0, 0, 0], [0, 0, 0, 0],[0, 0, 0, 0]]}
+    xmin,ymin,xmax,ymax = roi_tag[0],roi_tag[1],roi_tag[2],roi_tag[3]
+    out_data["data"] = {"type": "digital",
+                        "values": ['0.0A', '0.0A' ,'0.0A'],
+                        "bboxes": [[int(xmin), int(ymin), int(xmax), int(0.667*ymin+0.333*ymax)],
+                                   [int(xmin), int(0.667*ymin+0.333*ymax), int(xmax), int(0.333*ymin+0.667*ymax)],
+                                   [int(xmin), int(0.333*ymin+0.667*ymax), int(xmax), int(ymax)]]}
     for res in result:
         bbox = res["bbox"]
-        content = res["content"]
-        cv2.rectangle(img_tag_, (int(bbox[0]), int(bbox[1])),
-                      (int(bbox[2]), int(bbox[3])), (0, 0, 255), thickness=2)
-        img_tag_ = img_chinese(img_tag_, deal_str(content), (int(bbox[0]), int(bbox[1] - 20)), (0, 0, 255), size=20)
-        if bbox[1]<ycenter and bbox[3]<ycenter:
-            out_data["data"]['values'][0]=deal_str(content)
-            out_data["data"]['bboxes'][0] = [int(bbox[0]), int(bbox[1]),int(bbox[2]), int(bbox[3])]
-        elif bbox[1]<ycenter and bbox[3]>ycenter:
-            out_data["data"]['values'][1]=deal_str(content)
-            out_data["data"]['bboxes'][1] = [int(bbox[0]), int(bbox[1]),int(bbox[2]), int(bbox[3])]
-        elif bbox[1]>ycenter and bbox[3]>ycenter:
-            out_data["data"]['values'][2]=deal_str(content)
-            out_data["data"]['bboxes'][2] = [int(bbox[0]), int(bbox[1]),int(bbox[2]), int(bbox[3])]
+        content = deal_str(res["content"])
+        # cv2.rectangle(img_tag_, (int(bbox[0]), int(bbox[1])),
+        #               (int(bbox[2]), int(bbox[3])), (0, 0, 255), thickness=2)
+        # img_tag_ = img_chinese(img_tag_, deal_str(content), (int(bbox[0]), int(bbox[1] - 20)), (0, 0, 255), size=20)
+        if content=='0.00' or content=='0.0' or content=='0.00A' or content=='0.0A':
+            for i in result:
+                i["content"] = '0.0A'
+            out_data["data"] = {"type": "digital",
+                                "values": ['0.0A', '0.0A', '0.0A'],
+                                "bboxes": [[int(xmin), int(ymin), int(xmax), int(0.667 * ymin + 0.333 * ymax)],
+                                           [int(xmin), int(0.667 * ymin + 0.333 * ymax), int(xmax),
+                                            int(0.333 * ymin + 0.667 * ymax)],
+                                           [int(xmin), int(0.333 * ymin + 0.667 * ymax), int(xmax), int(ymax)]]}
+                # if i["bbox"][1] < ycenter and i["bbox"][3] < ycenter:
+                #     out_data["data"]['values'][0] = deal_str(i["content"])
+                #     out_data["data"]['bboxes'][0] = [int(i["bbox"][0]), int(i["bbox"][1]), int(i["bbox"][2]), int(i["bbox"][3])]
+                # elif i["bbox"][1] < ycenter and i["bbox"][3] > ycenter:
+                #     out_data["data"]['values'][1] = deal_str(i["content"])
+                #     out_data["data"]['bboxes'][1] = [int(i["bbox"][0]), int(i["bbox"][1]), int(i["bbox"][2]), int(i["bbox"][3])]
+                # elif i["bbox"][1] > ycenter and i["bbox"][3] > ycenter:
+                #     out_data["data"]['values'][2] = deal_str(i["content"])
+                #     out_data["data"]['bboxes'][2] = [int(i["bbox"][0]), int(i["bbox"][1]), int(i["bbox"][2]), int(i["bbox"][3])]
+        else:
+            if bbox[1]<ycenter and bbox[3]<ycenter:
+                out_data["data"]['values'][0]=deal_str(content)
+                out_data["data"]['bboxes'][0] = [int(bbox[0]), int(bbox[1]),int(bbox[2]), int(bbox[3])]
+            elif bbox[1]<ycenter and bbox[3]>ycenter:
+                out_data["data"]['values'][1]=deal_str(content)
+                out_data["data"]['bboxes'][1] = [int(bbox[0]), int(bbox[1]),int(bbox[2]), int(bbox[3])]
+            elif bbox[1]>ycenter and bbox[3]>ycenter:
+                out_data["data"]['values'][2]=deal_str(content)
+                out_data["data"]['bboxes'][2] = [int(bbox[0]), int(bbox[1]),int(bbox[2]), int(bbox[3])]
+
+    # if len(result)<3:
+    for i in range(3):
+        cv2.rectangle(img_tag_, (int(out_data["data"]['bboxes'][i][0]), int(out_data["data"]['bboxes'][i][1])),
+                      (int(out_data["data"]['bboxes'][i][2]), int(out_data["data"]['bboxes'][i][3])), (0, 0, 255), thickness=2)
+        img_tag_ = img_chinese(img_tag_, deal_str(out_data["data"]['values'][i]), (int(out_data["data"]['bboxes'][i][0]), int(out_data["data"]['bboxes'][i][1] - 20)), (0, 0, 255), size=20)
+
+
 
     ## 可视化计算结果
     f = open(os.path.join(save_path, TIME_START + "out_data.json"), "w")
     json.dump(out_data, f, ensure_ascii=False, indent=2)  # 保存输入信息json文件
     f.close()
 
-    #cv2.imwrite(os.path.join(save_path, TIME_START + "img_tag_cfg.jpg"), img_tag_)
+    # cv2.imwrite(os.path.join(save_path, TIME_START + "img_tag_cfg.jpg"), img_tag_)
     cv2.imwrite(os.path.join( TIME_START + "img_tag_cfg.jpg"), img_tag_)
 
     ## 输出可视化结果的图片。
