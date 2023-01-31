e23727cb5602de79d470c30c417c473e5151a0c5
linduaner <linduaner@Ut.cn>
2023-01-31 15:39:11 +0800

    numba jit加速

--- python_codes/lib_decode_model.py
+++ python_codes/lib_decode_model.py
@@ -1,8 +1,10 @@
 import random
 import io
 import torch
+import numba
 
 # 解密
+@numba.jit
 def decode_model(encode_model_name, keys=1,length=5000):
     f = open(encode_model_name, "rb")
     encode_model = f.read()
@@ -13,13 +15,11 @@ def decode_model(encode_model_name, keys=1,length=5000):
     for i in range(len(encode_model)):
         encode_model[i] ^= byteskey
     decode_model_ = bytes(encode_model)
-    decode_model_ = io.BytesIO(decode_model_)
+    # decode_model_ = io.BytesIO(decode_model_)  #返回io对象
     return decode_model_
 
 if __name__ == '__main__':
-    #img_rotate_batch("/data/PatrolAiImg2022/bjbmyw")
     filename = 'hs_bj_infer_encode.pt'
-    # filename = 'hs_bj_infer_encode.txt'
     str_4 = decode_model('hs_bj_infer_encode.pt')
     print(type(str_4))
     test = torch.load(str_4)
