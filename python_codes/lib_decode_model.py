b1844427fd68921a42367bde526ca6aa7b2e216f
linduaner <linduaner@Ut.cn>
2023-01-31 11:28:28 +0800

    模型解密模块

---
+++ python_codes/lib_decode_model.py
@@ -0,0 +1,26 @@
+import random
+import io
+import torch
+
+# 解密
+def decode_model(encode_model_name, keys=1,length=5000):
+    f = open(encode_model_name, "rb")
+    encode_model = f.read()
+    f.close()
+    random.seed(keys)
+    encode_model = bytearray(encode_model)
+    byteskey = random.randint(0, 255)
+    for i in range(len(encode_model)):
+        encode_model[i] ^= byteskey
+    decode_model_ = bytes(encode_model)
+    decode_model_ = io.BytesIO(decode_model_)
+    return decode_model_
+
+if __name__ == '__main__':
+    #img_rotate_batch("/data/PatrolAiImg2022/bjbmyw")
+    filename = 'hs_bj_infer_encode.pt'
+    # filename = 'hs_bj_infer_encode.txt'
+    str_4 = decode_model('hs_bj_infer_encode.pt')
+    print(type(str_4))
+    test = torch.load(str_4)
+
