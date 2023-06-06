212d28d4cc651e8dec87ca8d65a1f4392c23b2bc
yuanhui <yuanhui@ut.cn>
2023-06-06 15:48:43 +0800

    修改输出的int64改为int32

--- python_codes/app_match.py
+++ python_codes/app_match.py
@@ -15,8 +15,8 @@ def patrol_match(input_data):
 
     out_pointers = {}
     for scale in pointers:
-        coor = pointers[scale]
-        out_pointers[scale] = convert_coor(coor, M)
+        coor = convert_coor(pointers[scale], M)
+        out_pointers[scale] = (int(coor[0]), int(coor[1]))
     
     out_bboxes = {}
     for label in bboxes:
@@ -26,7 +26,7 @@ def patrol_match(input_data):
         c_ = np.array(coors_, dtype=int)
         H, W = img_tag.shape[:2]
         r = [min(c_[:,0]), min(c_[:, 1]), max(c_[:,0]), max(c_[:,1])]
-        box_ = [max(0, r[0]), max(0, r[1]), min(W, r[2]), min(H, r[3])]
+        box_ = [int(max(0, r[0])), int(max(0, r[1])), int(min(W, r[2])), int(min(H, r[3]))]
         out_bboxes[label] = box_
     
     img_tag_ = img_tag.copy()
@@ -37,8 +37,8 @@ def patrol_match(input_data):
     
     for label in out_bboxes:
         o_ = out_bboxes[label]
-        cv2.rectangle(img_ref, (int(o_[0]), int(o_[1])),(int(o_[2]), int(o_[3])), (255, 0, 255), thickness=1)
-        cv2.putText(img_ref, label, (int(o_[0]), int(o_[1])),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), thickness=1)
+        cv2.rectangle(img_tag_, (int(o_[0]), int(o_[1])),(int(o_[2]), int(o_[3])), (255, 0, 255), thickness=1)
+        cv2.putText(img_tag_, label, (int(o_[0]), int(o_[1])),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), thickness=1)
 
     out_data = {"code":0, "data": {"pointers": out_pointers, "bboxes": out_bboxes}, "img_result":img2base64(img_tag_), "msg": "Success!"}
     return out_data
