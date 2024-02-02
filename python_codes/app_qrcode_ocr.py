15bb588521ab9cf062f92d6110bde6627993ec80
yuanhui <yuanhui@ut.cn>
2024-02-02 10:46:37 +0800

    update

--- python_codes/app_qrcode_ocr.py
+++ python_codes/app_qrcode_ocr.py
@@ -91,7 +91,13 @@ def inspection_qrcode(input_data):
     
     ## 输出可视化结果的图片。
     img_tag_ = img_chinese(img_tag_, out_data["msg"], (10, 130), color=(255, 0, 0), size=30)
-    out_data["img_result"] = img2base64(img_tag_)
+    
+    if os.path.exists(input_data["image"]): 
+        out_file = input_data["image"][:-4] + "_result.jpg"
+        cv2.imwrite(out_file, img_tag_)
+        out_data["img_result"] = out_file
+    else:
+        out_data["img_result"] = img2base64(img_tag_)
 
     return out_data
 
