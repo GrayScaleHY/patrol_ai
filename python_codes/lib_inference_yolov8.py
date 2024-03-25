470e0e058531f1836ae075889ed627aa96f377f6
yuanhui <yuanhui@ut.cn>
2024-03-25 11:59:12 +0800

    添加yolov8关键点推理功能

--- python_codes/lib_inference_yolov8.py
+++ python_codes/lib_inference_yolov8.py
@@ -4,7 +4,7 @@ https://docs.ultralytics.com/python/
 """
 
 from ultralytics import YOLO
-import ultralytics
+from ultralytics.utils.ops import scale_image
 import numpy as np
 from lib_rcnn_ops import filter_cfgs
 import cv2
@@ -46,16 +46,15 @@ def inference_yolov8(model,
         cfgs: 预测结果，格式为[{"label": "", "coor": [x0, y0, x1, y1], "score": float, "mask": numpy}, {}, ..]
     """
     labels = model.names  # 标签名
-    img_ = img.copy()
     
     if resize is None:
         resize = model.model.args["imgsz"]
 
-    result = model(img_, iou=same_iou_thres, conf=conf_thres, imgsz=resize)[0]  # 推理结果
+    result = model(img, iou=same_iou_thres, conf=conf_thres, imgsz=resize)[0]  # 推理结果
     task = model.task  # 模型类型, detect, segment, classify
-    H, W = img.shape[:2]
+    
 
-    assert task in ["detect", "segment", "classify"], "is not yolov8 model !"
+    assert task in ["detect", "segment", "classify", "pose"], "is not yolov8 model !"
 
     # 分类模型推理结果后处理
     if task == "classify":
@@ -70,42 +69,43 @@ def inference_yolov8(model,
         return cfgs
 
     # 检测和分割模型推理结果后处理
-    res_boxes = result.boxes.cpu().numpy()
-    if str(ultralytics.__version__) == "8.0.51":
-        boxes = res_boxes.boxes
-    else:
-        boxes = res_boxes.data
+    res_boxes = result.boxes.data.cpu().numpy()
+    boxes = res_boxes.data
     if len(boxes) == 0:
         return[]
     
     if task == "segment":
-        res_masks = result.masks.cpu().numpy()
-        res_segments = result.masks.segments
+        res_masks = result.masks.data.cpu().numpy()
+        # res_masks_ = scale_image(res_masks, result.orig_shape)
+        res_segments = result.masks.xyn
+    
+    if task == "pose":
+        rec_keys = result.keypoints.xy.cpu().numpy()
+
 
     cfgs = []
-    img_shape = np.array([W, H])
-    for i, lab in enumerate(res_boxes.cls):
-        b = list(res_boxes.xyxy[i])  # 目标框坐标
+    for i, b in enumerate(res_boxes):
         box = [int(b[0]), int(b[1]), int(b[2]), int(b[3])]
-        label = labels[lab]  # 标签
-        score = res_boxes.conf[i]  # 得分
+        label = labels[b[-1]]  # 标签
+        score = b[-2]
 
         if task == "segment":
-            if str(ultralytics.__version__) == "8.0.51":
-                mask = res_masks.masks[i].astype(np.uint8)  # 分割模型的mask
-            else:
-                mask = res_masks.data[i].astype(np.uint8)  # 分割模型的mask
-            mask = cv2.resize(mask, (W, H)) # 将mask映射为原始图片大小
-            segments = res_segments[i] * img_shape
+            mask = scale_image(res_masks[i], result.orig_shape)[:,:,0]
+            segments = res_segments[i] * np.array(list(result.orig_shape))
             segments = segments.astype(int)
             if np.sum(segments) == 0: # 没有mask的情况
                 continue
         else:
             mask = None
             segments = None
+        
+        if task == "pose":
+            keys = rec_keys[i].astype(int)
+        else:
+            keys = None
 
         cfgs.append({"label": label, "coor": box,
-                    "score": score, "mask": mask, "segments": segments})
+                    "score": score, "mask": mask, "segments": segments, "keys": keys})
 
     if len(cfgs) == 0:
         return []
@@ -122,8 +122,9 @@ if __name__ == '__main__':
     import cv2
     import time
 
-    img_file = "/data/PatrolAi/result_patrol/pointer/0615135355_2号主变2号低抗线温表-视频_tag.jpg"
-    weight = "/data/PatrolAi/yolov8/pointer.pt"
+    img_file = "/data/PatrolAi/patrol_ai/python_codes/images/#5187_MaxZoom_1_meter.jpg"
+    weight = "/data/home/yh/pose/runs/pose/train11/weights/best.pt"
+    # weight = "/data/PatrolAi/yolov8/pointer.pt"
     img = cv2.imread(img_file)
     img_raw = img.copy()
     print("input image shape:", img.shape)
@@ -137,18 +138,21 @@ if __name__ == '__main__':
                             same_iou_thres=0.8, diff_iou_thres=1, focus_labels=None)
     print("inference image spend time:", time.time() - start)
 
-    for i in range(len(cfgs)):
-        tmp = cfgs[i]
+    for cfg in cfgs:
         # print(tmp)
-        c = tmp["coor"]
-        label = tmp["label"]
-        score = str(tmp["score"]).zfill(3)
+        c = cfg["coor"]
+        label = cfg["label"]
+        score = str(cfg["score"]).zfill(3)
         # mask = tmp['mask']
         # print(mask)
         # cv2.polylines(img, [mask], isClosed=True, color=(0, 0, 255), thickness=1)
-        cv2.rectangle(img_raw, (int(c[0]), int(c[1])),
-                      (int(c[2]), int(c[3])), (0, 255, 0), 2)
-        cv2.putText(img_raw, label + ": " + score, (int(c[0]), int(
-            c[1]) + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), thickness=1)
+        keys = cfg["keys"]
+        for key_ in keys:
+            cv2.circle(img_raw, (int(key_[0]), int(key_[1])), 1, (255, 0, 255), 8)
+
+        # cv2.rectangle(img_raw, (int(c[0]), int(c[1])),
+        #               (int(c[2]), int(c[3])), (0, 255, 0), 2)
+            cv2.putText(img_raw, label + ": " + score, (int(key_[0]), int(
+                key_[1]) + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), thickness=1)
 
     cv2.imwrite('bus_result.jpg', img_raw)
